94a87c3cd526063f136d465476519b3f03042c9f
Diogo Ferreira Fernandes <dio@mac.local>
2020-02-28 15:32:26 -0300

    Versionate react on import map, generate client import map

--- helpers/bundle_javascript.js
+++ helpers/bundle_javascript.js
@@ -1,14 +1,6 @@
-/**
- * Bundle to do list:
- * - Generate import_map.json
- * - Create /public/.scr/main.js from /client/main.js
- * - Duplicate every javascript file inside /src to /public/.src
- * - If possible, minify files
- */
+import { writeFileStr, exists, walk, copy } from "fs";
 
-import { exists, walk, copy } from "fs";
-
-// Walk thought pages
+// Walk thought javascript files
 for await (const file of walk("./src")) {
   if (file.info._isFile) {
     const permitedFiles = [
@@ -30,15 +22,30 @@ for await (const file of walk("./src")) {
 
       const folderExist = await exists(destinyFolder);
       if (folderExist) {
-        await copy(origin, destiny);
+        await copy(origin, destiny, { overwrite: true });
       } else {
         await Deno.mkdir(destinyFolder, { recursive: true });
-        await copy(origin, destiny);
+        await copy(origin, destiny, { overwrite: true });
 
       }
-      // console.log(`${ destiny } generated`)
+      console.log(`${ destiny } generated`);
     }
   }
 }
+
+// Copy main.js
+await copy('./client/main.js', './public/.src/main.js', { overwrite: true });
+console.log("./public/.src/main.js generated");
+
+// Generate import_map.json
+const importMap = {
+  imports: {
+    "react": "https://dev.jspm.io/react@16.12.0",
+    "react-dom": "https://dev.jspm.io/react-dom@16.12.0",
+    "react-router-dom": "https://cdn.pika.dev/react-router-dom@^5.1.2"
+  }
+};
+await writeFileStr('./public/import_map.json', JSON.stringify(importMap, null, 2));
+console.log("./public/import_map.json generated");
 
 export default {};
