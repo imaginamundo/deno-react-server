7176d362169e81cd11cbbe2ee28da2bb43940bb8
Diogo Ferreira Fernandes <dio@mac.local>
2020-05-14 19:51:19 -0300

    Remove hook test from pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React/*, { useState }*/ from "react";
 import Menu from "../components/Menu.jsx";
 
 function Page({ props }) {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
   
   return (
     <>
@@ -36,7 +36,7 @@ export default Page;`}
         getInitialProps function.
       </p>
       <pre>{JSON.stringify(props, null, 2)}</pre>
-      <h2>useState hook test</h2>
+      {/* <h2>useState hook test</h2>
       <p>
         Clicked {counter} times.
       </p>
@@ -44,7 +44,7 @@ export default Page;`}
         <button onClick={() => setCounter(counter + 1)}>
           Add clicks
         </button>
-      </p>
+      </p> */}
     </>
   );
 }
